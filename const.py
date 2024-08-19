f09ed07df932103f9ba7a67b1b81bdbaa03999bf
RaghunathDewal <Rsdewal15@gmail.com>
2024-08-19 12:39:39 +0530

    Update const.py

--- const.py
+++ const.py
@@ -4,7 +4,7 @@ PAGE_SIZE = 4096
 writeInsideReadTxErr = Exception("can't perform a write operation inside a read transaction")
 COLLECTION_SIZE = 128 
 COUNTER_SIZE = 128 
-MAGIC_NUMBER = 0xD00DB00D
+MAGIC_NUMBER = 0xC0DECAFE
 MAGIC_NUMBER_SIZE = 4 
 
  
